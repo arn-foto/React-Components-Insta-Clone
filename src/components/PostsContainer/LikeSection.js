5b3fc7eefdb302fff6d13f220c59dde2ffe25e86
arn-foto <38259824+arn-foto@users.noreply.github.com>
2020-05-13 12:03:03 -0700

    Finished 1 stretch goal

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,52 +1,32 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React, {
-  useState
-} from "react";
-import {
-  FontAwesomeIcon
-} from '@fortawesome/react-fontawesome'
-import {
-  faComment,
-  faHeart
-} from '@fortawesome/free-regular-svg-icons'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
-const LikeSection = props => {
-  // const [Likes, setLikes] = useState(0);
-  // const addLike = () => {
-  //   setLikes(Likes + 1);
-  // };
-  return ( <
-    div >
-    <
-    div className = "like-section"
-    key = "likes-icons-container" >
-    <
-    div className = "like-section-wrapper" >
-    <
-    FontAwesomeIcon icon = {
-      faHeart
-    }
-    onClick = {
-      props.clicker
-    }
-    /> <
-    /div> <
-    div className = "like-section-wrapper" >
-    <
-    FontAwesomeIcon icon = {
-      faComment
-    }
-    /> <
-    /div> <
-    /div> <
-    p className = "like-number" > {
-      props.Likes
-    }
-    Likes < /p> <
-    /div>
-  )
+const LikeSection = (props) => {
+	// const [Likes, setLikes] = useState(0);
+	// const addLike = () => {
+	//   setLikes(Likes + 1);
+	// };
+	return (
+		<div>
+			<div className="like-section" key="likes-icons-container">
+				<div className="like-section-wrapper">
+					<FontAwesomeIcon icon={faHeart} onClick={props.clicker} />{' '}
+				</div>{' '}
+				<div className="like-section-wrapper">
+					<FontAwesomeIcon icon={faComment} />{' '}
+				</div>{' '}
+			</div>{' '}
+			<div className="like-number">
+				{' '}
+				{props.Likes}
+				Likes{' '}
+			</div>{' '}
+		</div>
+	);
 };
 
 export default LikeSection;
